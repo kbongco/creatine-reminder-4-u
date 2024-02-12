efc0a1139ac80641971e838fa4b6454e8e3ce13f
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-02-12 13:11:03 -0500

    completed welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,15 @@ import './App.css'
 import Button from './components/Button/Button'
 import { ButtonSizes } from './enums/component-enums'
 import Card from './components/Card/Card'
+import MainContainer from './Containers/MainContainer'
 
 function App() {
 
   return (
     <>
-      <h1>Test of font styles</h1>
       <div>
         <div>
-          {/* <Card size='default'/> */}
-          <Card size='small-wide'/>
+          <MainContainer/>
         </div>
         </div>
     </>
---
+++ src/Containers/MainContainer.tsx
@@ -0,0 +1,9 @@
+import Welcome from "../Screens/Welcome/Welcome";
+
+export default function MainContainer() {
+  return (
+    <>
+      <Welcome/>
+    </>
+  )
+}
