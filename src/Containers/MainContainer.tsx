148676e083b62b41184d966fcd8a5fe02d61176a
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-02-21 11:13:05 -0500

    start points

--- src/Containers/MainContainer.tsx
+++ src/Containers/MainContainer.tsx
@@ -1,9 +1,16 @@
 import Welcome from "../Screens/Welcome/Welcome";
+import { useRoutes } from 'react-router-dom';
+import Login from "../Screens/Login/Login";
 
 export default function MainContainer() {
   return (
     <>
-      <Welcome/>
+      <Welcome />
+      {
+        useRoutes([
+          { path: '/login', element: <Login/> }
+        ])
+      }
     </>
   )
 }
