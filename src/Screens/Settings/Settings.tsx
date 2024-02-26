1b2dbdd21d9743003b1d23a72564c70c74e014ec
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-02-26 12:39:09 -0500

    added in settings options

--- src/Screens/Settings/Settings.tsx
+++ src/Screens/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import { faBell, faJar, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faBookOpenReader, faComment, faJar, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Settings.scss';
 
@@ -11,31 +11,56 @@ export default function Settings() {
             <h1>Settings</h1>
           </div>
         </div>
-
-        <div className='settings-page-options-container'>
-          <div className='settings-page-options-icons'>
-            <div className='settings-icon-container'>
-              {<FontAwesomeIcon icon={faUser} />}
+        <div className='settings-options-grid-container'>
+          <div className='settings-page-options-container'>
+            <div className='settings-page-options-icons'>
+              <div className='settings-icon-container'>
+                {<FontAwesomeIcon icon={faUser} />}
+              </div>
             </div>
-          </div>
-          <div className='settings-page-options-text'>
-            <p>Profile Settings</p>
-          </div>
-          <div className='settings-page-options-icons'>
-            <div className='settings-icon-container'>
-              <FontAwesomeIcon icon={faBell} />
+            <div className='settings-page-options-text'>
+              <p>Profile Settings</p>
             </div>
-          </div>
-          <div className='settings-page-options-text'>
-            <p>Notification Settings</p>
-          </div>
-          <div className='settings-page-options-icons'>
-            <div className='settings-icon-container'>
-              <FontAwesomeIcon icon={faJar} />
+            <div className='settings-page-options-icons'>
+              <div className='settings-icon-container'>
+                <FontAwesomeIcon icon={faBell} />
+              </div>
             </div>
-          </div>
-          <div className='settings-page-options-text'>
-            <p>Current Creatine</p>
+            <div className='settings-page-options-text'>
+              <p>Notification Settings</p>
+            </div>
+            <div className='settings-page-options-icons'>
+              <div className='settings-icon-container'>
+                <FontAwesomeIcon icon={faJar} />
+              </div>
+            </div>
+            <div className='settings-page-options-text'>
+              <p>Current Creatine</p>
+            </div>
+            <div className='settings-page-options-icons'>
+              <div className='settings-icon-container'>
+                <FontAwesomeIcon icon={faComment} />
+              </div>
+            </div>
+            <div className='settings-page-options-text'>
+              <p>Leave Feedback</p>
+            </div>
+            <div className='settings-page-options-icons'>
+              <div className='settings-icon-container'>
+                <FontAwesomeIcon icon={faBookOpenReader} />
+              </div>
+            </div>
+            <div className='settings-page-options-text'>
+              <p>Learn More</p>
+            </div>
+            <div className='settings-page-options-icons'>
+              <div className='settings-icon-container'>
+                <FontAwesomeIcon icon={faRightFromBracket} />
+              </div>
+            </div>
+            <div className='settings-page-options-text'>
+              <p>Log out</p>
+            </div>
           </div>
         </div>
       </div>
