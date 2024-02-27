4ff3e06161d217cc0d48142baa5e47ad28a32bd6
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-02-27 15:06:25 -0500

    fixed up UI for forgot password, not to add functionality

--- src/Containers/MainContainer.tsx
+++ src/Containers/MainContainer.tsx
@@ -6,6 +6,7 @@ import Home from "../Screens/HomeDashboard/Home";
 import Settings from "../Screens/Settings/Settings";
 import { useEffect, useState } from "react";
 import { auth } from "../config/firebase";
+import Forgot from "../Screens/ForgotPassword/ForgotPassword";
 
 export default function MainContainer() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -25,6 +26,7 @@ export default function MainContainer() {
           { path: '/sign-up', element: <Signup /> },
           currentUser ? { path: '/home', element: <Home /> } : { path: '/home', element: <Navigate to="/login" /> },
           currentUser ? { path: '/settings', element: <Settings /> } : { path: '/settings', element: <Navigate to="/login" /> },
+          {path: '/forgot-password', element: <Forgot/>}
           // { path: '/home', element: <Home /> },
           // { path: '/settings', element: <Settings /> }
         ])
---
+++ src/Screens/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Card from "../../components/Card/Card";
+import Input from "../../components/Input/Input";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "../../components/Button/Button";
+import { ButtonSizes } from "../../enums/component-enums";
+import './ForgotPassword.scss';
+
+export default function Forgot() {
+  const [email, setEmail] = useState('');
+  const forgotTitle = 'Forgot Password';
+
+  const emailOnChange = (e) => {
+    setEmail(e.target.value);
+  }
+  
+  const findPassword = (e) => {
+    console.log('test')
+  }
+  const forgotBody = (
+    <div className='forgot-password-body'>
+      <p>Forgot to take you creatine and your password? We got you covered!</p>
+      <div className='forgot-password-input-container'>
+      <Input
+            inputStyle="large-and-wide"
+            icon={<FontAwesomeIcon icon={faEnvelope} />}
+            onChange={emailOnChange}
+          />
+        <div className='forgot-password-button'>
+        <Button
+            label='Sign in'
+            size={ButtonSizes.Default}
+            onClick={findPassword}
+          />
+          
+          </div>
+      </div>
+    </div>
+  )
+  
+  return (
+    <>
+      <div className='forgot-password-container'>
+        <div className='forgot-password-card-container'>
+          <Card
+            size='medium-card'
+            header={forgotTitle}
+            body={forgotBody}
+            footer='test'
+          />
+          </div>
+      </div>
+    </>
+  )
+}
